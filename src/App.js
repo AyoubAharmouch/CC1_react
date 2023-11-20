c89455d2aed17e36d724afee7214834cc7364f7d
AyoubAharmouch <ayoub.aharmouch305@gmail.com>
2023-11-20 09:28:31 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import './App.css';
-import {Route,Routes} from 'react-router-dom'
-import Nav from './comp/Navv';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Nav from "./comp/Navv";
 import Cal from "./comp/Cal";
-import Slider from "./comp/Slider"
-import TodoList from './comp/Todo';
-import Api from './comp/Api';
+import Slider from "./comp/Slider";
+import TodoList from "./comp/Todo";
+import Api from "./comp/Api";
 
-import { LangProvider } from './comp/Langcontext';
-import Hause from './comp/Hause';
+import { LangProvider } from "./comp/Langcontext";
+import Hause from "./comp/Hause";
 
 function App() {
   return (
     <div className="App">
-     <Nav />
-     <LangProvider>
-      <Routes>
-        <Route path="/home" element={
-            <Hause />
-        } />
-        <Route path='/calculator' element={<Cal />} />
-        <Route path='/Slider' element={<Slider />} />
-        <Route path='/todo' element={<TodoList />} />
-        <Route path='/API' element={<Api />} />
-      </Routes>
+      <Nav />
+      <LangProvider>
+        <Routes>
+          <Route path="/" element={<Hause />} />
+          <Route path="/home" element={<Hause />} />
+          <Route path="/calculator" element={<Cal />} />
+          <Route path="/Slider" element={<Slider />} />
+          <Route path="/todo" element={<TodoList />} />
+          <Route path="/API" element={<Api />} />
+        </Routes>
       </LangProvider>
     </div>
   );
